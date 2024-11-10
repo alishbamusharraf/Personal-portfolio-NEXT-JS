b1e469ca81cf3bdb0b4ec6b325e7b1653ab5f4e2
ALISHBA MUSHARRAF <162012518+alishbamusharraf@users.noreply.github.com>
2024-11-10 14:55:36 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../my-app/src/app/components/footer'
+import Skills from '../my-app/src/app/components/skills'
+const page = () => {
+    return (
+      <div>
+    <Footer/>
+    <Skills/>
+     </div>
+    )
+  }
+
+export default page
